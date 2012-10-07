54dbd90f425d944296088dc5968690f7230effed
Violeta Georgieva <milesg78@gmail.com>
2012-10-07 18:43:49 +0300

    bug 391323: Update javax.persistence dependency to version 2.0.3.x

--- build-web-container/copyJavaxToOsgi.sh
+++ build-web-container/copyJavaxToOsgi.sh
@@ -6,7 +6,7 @@ cp $SRC/javax.annotation-1.1.0.v201108011116.jar $REPO/javax.annotation/javax.an
 cp $SRC/javax.ejb-3.0.0.v201203061215.jar $REPO/javax.ejb/javax.ejb-3.0.0.v201203061215.jar
 cp $SRC/javax.el-2.2.0.v201108011116.jar $REPO/javax.el/javax.el-2.2.0.v201108011116.jar
 cp $SRC/javax.mail-1.4.0.v201005080615.jar $REPO/javax.mail/javax.mail-1.4.0.v201005080615.jar
-cp $SRC/com.springsource.javax.persistence-1.0.0.jar $REPO/com.springsource.javax.persistence/com.springsource.javax.persistence-1.0.0.jar
+cp $SRC/javax.persistence-2.0.3.v201010191057.jar $REPO/javax.persistence/javax.persistence-2.0.3.v201010191057.jar
 cp $SRC/javax.servlet-3.0.0.v201103241009.jar $REPO/javax.servlet/javax.servlet-3.0.0.v201103241009.jar
 cp $SRC/javax.servlet.jsp-2.2.0.v201112011158.jar $REPO/javax.servlet.jsp/javax.servlet.jsp-2.2.0.v201112011158.jar
 cp $SRC/javax.transaction-1.1.0.v201205091237.jar $REPO/javax.transaction/javax.transaction-1.1.0.v201205091237.jar
