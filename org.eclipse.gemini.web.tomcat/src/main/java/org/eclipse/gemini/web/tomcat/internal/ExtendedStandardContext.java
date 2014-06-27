97c826b1c450ef81b666e7e82a10f301ea0323b9
Violeta Georgieva <milesg78@gmail.com>
2014-06-27 23:59:25 +0300

    bug 431969: Support StandardContext.reload.

---
+++ org.eclipse.gemini.web.tomcat/src/main/java/org/eclipse/gemini/web/tomcat/internal/ExtendedStandardContext.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2014 SAP AG
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at
+ *   http://www.opensource.org/licenses/apache2.0.php.
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *   Violeta Georgieva - initial contribution
+ *******************************************************************************/
+
+package org.eclipse.gemini.web.tomcat.internal;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.catalina.core.StandardContext;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.wiring.FrameworkWiring;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Extends the Tomcat {@link StandardContext} to add custom functionality.
+ *
+ *
+ */
+public class ExtendedStandardContext extends StandardContext {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ExtendedStandardContext.class);
+
+    private Bundle bundle;
+
+    public ExtendedStandardContext(Bundle bundle) {
+        super();
+        this.bundle = bundle;
+    }
+
+	/**
+     * Returns <code>true</code> for exploded bundles.
+     */
+    @Override
+    public boolean isFilesystemBased() {
+        String docBase = getDocBase();
+        File f = new File(docBase);
+        return f.isDirectory();
+    }
+
+	@Override
+    public synchronized void reload() {
+        if (!getState().isAvailable())
+            throw new IllegalStateException
+                ("Context with name [" + getName() + "] has not yet been started.");
+
+        if(LOGGER.isInfoEnabled())
+            LOGGER.info("Reloading Context with name [" + getName() + "] has started.");
+
+		final Bundle systemBundle = this.bundle.getBundleContext().getBundle(0);
+		final FrameworkWiring frameworkWiring = systemBundle.adapt(FrameworkWiring.class);
+		Set<Bundle> bundles = new HashSet<Bundle>();
+		bundles.add(this.bundle);
+		frameworkWiring.refreshBundles(bundles);
+
+        if(LOGGER.isInfoEnabled())
+            LOGGER.info(sm.getString("Reloading Context with name [" + getName() + "] is completed"));
+
+    }
+
+}
